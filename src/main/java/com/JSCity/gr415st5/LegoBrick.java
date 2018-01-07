a448a850f8f53e253797ba09d615521b15472f50
nadezdakislova <nadezdakislova94@gmail.com>
2018-01-07 18:52:51 +0100

    Added my files

--- src/main/java/com/JSCity/gr415st5/LegoBrick.java
+++ src/main/java/com/JSCity/gr415st5/LegoBrick.java
@@ -1,90 +1,66 @@
 package com.JSCity.gr415st5;
-
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
-import eu.printingin3d.javascad.coords.Coords3d;
-import eu.printingin3d.javascad.coords.Dims3d;
-import eu.printingin3d.javascad.enums.Side;
+import eu.printingin3d.javascad.coords.*;
 import eu.printingin3d.javascad.models.Abstract3dModel;
-import eu.printingin3d.javascad.models.Cube;
-import eu.printingin3d.javascad.models.Cylinder;
+import eu.printingin3d.javascad.models.Sphere;
+import eu.printingin3d.javascad.models.*;
 import eu.printingin3d.javascad.tranzitions.Difference;
 import eu.printingin3d.javascad.tranzitions.Union;
+import eu.printingin3d.javascad.coords.Coords3d;
 
-public class LegoBrick extends Union {
-	private static final double ONE_SEGMENT_WIDTH = 8.0;
-	private static final double HEIGHT = 9.6;
-	private static final double HORIZONTAL_GAP = 2*0.1;
-	private static final double WALL_THICKNESS = 1.5;
-	private static final double AXLE_INNER_DIAMETER = 4.75;
-	private static final double AXLE_OUTER_DIAMETER = 6.51;
-	private static final double AXLE_ONE_DIAMETER = 3.0;
-	private static final double KNOB_DIAMETER = 4.85;
-	private static final double KNOB_HEIGTH = 1.8;
-	
-	public LegoBrick(int xSize, int ySize) {
-		super(getModels(xSize, ySize));
+class LegoBrick extends Union {
+	LegoBrick() {
+		super(getModels());
 	}
 
-	private static List<Abstract3dModel> getModels(int xSize, int ySize) {
-		List<Abstract3dModel> models = new ArrayList();
-		Difference base = new Difference(
-						new Cube(new Dims3d(ONE_SEGMENT_WIDTH*xSize-HORIZONTAL_GAP, ONE_SEGMENT_WIDTH*ySize-HORIZONTAL_GAP, HEIGHT)),
-						new Cube(new Dims3d(ONE_SEGMENT_WIDTH*xSize-HORIZONTAL_GAP-WALL_THICKNESS*2, ONE_SEGMENT_WIDTH*ySize-HORIZONTAL_GAP-WALL_THICKNESS*2, HEIGHT-WALL_THICKNESS)).move(Coords3d.zOnly(-WALL_THICKNESS))						
-				);
-		models.add(base);
-		models.add(addAxles(xSize, ySize));
-		models.add(getKnobs(base, xSize, ySize));
-		return models;
-	}
+	private static List<Abstract3dModel> getModels() {
+		List<Abstract3dModel> models = new ArrayList<Abstract3dModel>();
+		Union first = new Union(
+				new Cylinder(180, 150).move(Coords3d.zOnly(20)),
+				new Cylinder(50, 100, 40).move(Coords3d.zOnly(110)),
+				new Cylinder(40, 200, 150).move(Coords3d.zOnly(40)),
+				new Cylinder(40, 200).move(Coords3d.zOnly(-65)),
+				new Cylinder(60, 50, 130).move(Coords3d.zOnly(-100)),
+				new Cylinder(20, 80).move(Coords3d.zOnly(-140)),
+				new Cube(new Dims3d(100, 250, 20)).move(new Coords3d(0, 300, -75))
 
-	private static Abstract3dModel getKnobs(Abstract3dModel base, int xSize, int ySize) {
-		List<Coords3d> moves = new ArrayList();
-		for (int x=0;x<xSize;x++) {
-			for (int y=0;y<ySize;y++) {
-				moves.add(new Coords3d((x-(xSize-1.0)/2.0)*ONE_SEGMENT_WIDTH, (y-(ySize-1.0)/2.0)*ONE_SEGMENT_WIDTH, 0.0));
-			}
-		}
-		return getKnob(base).moves(moves);
-	}
-	
-	private static Abstract3dModel addAxles(int xSize, int ySize) {
-		List<Coords3d> moves = new ArrayList();
-		if (xSize==1) {
-			for (int y=0;y<ySize-1;y++) {
-				moves.add(Coords3d.yOnly((y-(ySize-2.0)/2.0)*ONE_SEGMENT_WIDTH));
-			}
-			return getAxleOne().moves(moves);
-		}
-		else if (ySize==1) {
-			for (int x=0;x<xSize-1;x++) {
-				moves.add(Coords3d.xOnly((x-(xSize-2.0)/2.0)*ONE_SEGMENT_WIDTH));
-			}
-			return getAxleOne().moves(moves);
-		}
-		else {		
-			for (int x=0;x<xSize-1;x++) {
-				for (int y=0;y<ySize-1;y++) {
-					moves.add(new Coords3d((x-(xSize-2.0)/2.0)*ONE_SEGMENT_WIDTH, (y-(ySize-2.0)/2.0)*ONE_SEGMENT_WIDTH, 0.0));
-				}
-			}
-			return getAxle().moves(moves);
-		}
-	}
-	
-	private static Abstract3dModel getKnob(Abstract3dModel base) {
-		return new Cylinder(KNOB_HEIGTH, KNOB_DIAMETER/2.0).align(Side.TOP, base, false);
-	}
-	
-	private static Abstract3dModel getAxle() {
-		return new Difference(
-				new Cylinder(HEIGHT, AXLE_OUTER_DIAMETER/2.0),
-				new Cylinder(HEIGHT, AXLE_INNER_DIAMETER/2.0).move(Coords3d.zOnly(-0.01))
 		);
-	}
-	
-	private static Abstract3dModel getAxleOne() {
-		return new Cylinder(HEIGHT, AXLE_ONE_DIAMETER/2.0);
+
+		Difference second = new Difference(
+				first,
+				new Sphere(30).move(new Coords3d(0, 150, 110)),
+				new Sphere(30).move(new Coords3d(150, 0, 110)),
+				new Sphere(30).move(new Coords3d(0, -150, 110)),
+				new Sphere(30).move(new Coords3d(-150, 0, 110)),
+				new Sphere(30).move(new Coords3d(110, 110, 110)),
+				new Sphere(30).move(new Coords3d(-110, -110, 110)),
+				new Sphere(30).move(new Coords3d(-110, 110, 110)),
+				new Sphere(30).move(new Coords3d(110, -110, 110)),
+				new Cylinder(30, 190).move(Coords3d.zOnly(-55))
+		);
+		Union third = new Union(
+				second,
+				new Cylinder(40,150).move(Coords3d.zOnly(-60)),
+				new Cube(new Dims3d(5, 230, 25)).move(new Coords3d(-50, 310, -55)),
+				new Cube(new Dims3d(5, 230, 25)).move(new Coords3d(50, 310, -55)),
+				new Cube(new Dims3d(5, 150, 25)).rotate(new Angles3d(-40,0,0)).move(new Coords3d(-50, 190, -5)),
+				new Cube(new Dims3d(5, 150, 25)).rotate(new Angles3d(-40,0,0)).move(new Coords3d(50, 190, -5))
+		);
+		Difference forth = new Difference(
+				third,
+				new Cube(new Dims3d(50, 100, 80)).move(new Coords3d(0, 150, -30)),
+				new Cube(new Dims3d(15,15,15)).move(new Coords3d(150, 0, 0)),
+				new Cube(new Dims3d(15,15,15)).move(new Coords3d(0, -150, 0)),
+				new Cube(new Dims3d(15,15,15)).move(new Coords3d(-150, 0, 0)),
+				new Cube(new Dims3d(30,15,15)).move(new Coords3d(110, 110, 0)),
+				new Cube(new Dims3d(30,15,15)).move(new Coords3d(-110, -110, 0)),
+				new Cube(new Dims3d(30,15,15)).move(new Coords3d(-110, 110, 0)),
+				new Cube(new Dims3d(30,15,15)).move(new Coords3d(110, -110, 0))
+		);
+		models.add(forth);
+		return models;
 	}
 }
--- src/main/java/com/JSCity/gr415st5/Main.java
+++ src/main/java/com/JSCity/gr415st5/Main.java
@@ -7,7 +7,6 @@ import eu.printingin3d.javascad.exceptions.IllegalValueException;
 import eu.printingin3d.javascad.utils.SaveScadFiles;
 
 public class Main {
-
     /**
      * @param args
      * @throws IOException
@@ -15,10 +14,7 @@ public class Main {
      */
     public static void main(String[] args) throws IllegalValueException, IOException {
         new SaveScadFiles(new File(".\\target")).
-                addModel("lego_brick.scad", new LegoBrick(1, 6)).
+                addModel("LegoBrick.scad", new LegoBrick()).
                 saveScadFiles();
-        System.out.println("LegoBrick was created!");
-        System.out.println("You can find it in \"target\" directory");
-
     }
 }
